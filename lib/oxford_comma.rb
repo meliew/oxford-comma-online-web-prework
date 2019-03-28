b2b78104e7cbfd01e5c6e7879b8681b49acd9813
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-28 00:58:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  if array.length == 1
+    puts array
+  end 
+elsif array.length == 2
+  puts array.join(" and ")
+end
+elsif array.length == 3
+  puts array.join(" , and")
 
 end
