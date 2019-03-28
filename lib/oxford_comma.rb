faf8518f5718987cb9a1006a5c05e6f8a7548e87
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-28 00:59:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,5 @@ def oxford_comma(array)
   if array.length == 1
     puts array
   end
-elsif array.length == 2
-  puts array.join(" and ")
-end
-elsif array.length == 3
-  puts array.join(" , and")
-end
 
 end
